7493d5dbddefb029cf3ad15f278a6027567c8436
wayko621 <wayko@users.noreply.github.com>
2025-02-14 09:25:23 -0500

    created updateDisplayName.ps1

---
+++ updateDisplayName.ps1
@@ -0,0 +1,11 @@
+﻿$users = get-aduser -Filter *
+
+foreach($user in $users)
+{
+ $firstName = $user.GivenName
+ $lastName = $user.Surname
+ $newDisplayName = $firstName + ' ' + $lastName
+ $newDisplayName
+ set-aduser $user.SamAccountName -Replace @{displayName=$newDisplayName} 
+
+ }
