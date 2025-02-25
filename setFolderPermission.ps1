a2afe962e9075548621b685e1f4225319bae88ee
wayko621 <wayko@users.noreply.github.com>
2025-02-25 12:54:40 -0500

    Create setFolderPermission.ps1

---
+++ setFolderPermission.ps1
@@ -0,0 +1,17 @@
+﻿$Usersfolders = Get-ChildItem -Path D:\Test -Recurse -Directory  
+$papercut = "DEVDOGZ-PC\papercut"
+$fileSystemRights = "FullControl"
+$type = "Allow"
+
+$fileSystemAccessRuleArgumentList = $papercut, $fileSystemRights, $type
+
+$fileSystemAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($fileSystemAccessRuleArgumentList)
+
+
+
+foreach($folder in $Usersfolders)
+{
+    $NewAcl = Get-ACL -path $folder.FullName
+    $NewAcl.SetAccessRule($fileSystemAccessRule)
+    $newACL | Set-Acl $folder.FullName
+}
