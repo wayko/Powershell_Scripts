8d6461f9e20c585b5b5aef66b967f0749cefb804
wayko621 <wayko@users.noreply.github.com>
2025-03-04 08:23:03 -0500

    Create papercutAddPermission.ps1

---
+++ papercutAddPermission.ps1
@@ -0,0 +1,25 @@
+﻿$Usersfolders = Get-ChildItem -Path E:\ -Recurse -Directory -Depth 2
+$papercut = "CENTERLIGHT\zpapercut"
+$fileSystemRights = "FullControl"
+$type = "Allow"
+ 
+$fileSystemAccessRuleArgumentList = $papercut, $fileSystemRights, $type
+ 
+$fileSystemAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($fileSystemAccessRuleArgumentList)
+ 
+ 
+foreach($folder in $Usersfolders)
+{
+    if($folder.FullName -eq "E:\LaptopResources" -or $folder.FullName -eq "E:\Category" -or $folder.FullName -eq "E:\!ATOTest")
+    {
+        write-host "Skipping Folder"
+    }
+    else
+    {
+        $NewAcl = Get-ACL -path $folder.FullName
+        $NewAcl.SetAccessRule($fileSystemAccessRule)
+        $newACL | Set-Acl $folder.FullName
+        write-host "zpapercut has been given full access to " + $folder.FullName 
+    }
+    
+}
